faeb3b91c6f544457a70b1bc368ea8f6479b0c26
liuzhou <liuzhou@shineyue.com>
2022-02-11 20:11:34 +0800

    first commit

---
+++ src/main/java/com/example/lzmusic/controller/DefaultController.java
@@ -0,0 +1,16 @@
+package com.example.lzmusic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping
+    public String sayHello(){
+        return "欢迎进入音乐盒";
+    }
+}
---
+++ src/test/java/com/example/lzmusic/LzMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lzmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
